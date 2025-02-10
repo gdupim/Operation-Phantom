e4275c4b5c55a25f1d3690ca6e6b5c5f717c8dbf
Gabriel Dupim <gabrdupim@gmail.com>
2025-02-10 16:56:39 -0300

    sprites

--- src/main/java/Entidades/Player.java
+++ src/main/java/Entidades/Player.java
@@ -1,5 +1,11 @@
 package Entidades;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import Principal.Janela;
 import Principal.KeyHandler;
 
@@ -10,14 +16,85 @@ import Principal.KeyHandler;
 public class Player extends Entidade{
     Janela j;
     KeyHandler keyH;
-    
+
     public final int screenX;
     public final int screenY;
 
     public Player(Janela j, KeyHandler keyH) {
         this.j = j;
         this.keyH = keyH;
-        screenX = j.getScreenWidth()/2;
-        screenY = j.getScreenHeight()/2;
+        screenX = j.screenWidth/2;
+        screenY = j.screenHeight/2;
+
+        setDefaultValues();
+    }
+
+    public void setDefaultValues(){
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage(){
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/Player/snake/snake_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/Player/snake/snake_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/Player/snake/snake_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/Player/snake/snake_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/Player/snake/snake_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/Player/snake/snake_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/Player/snake/snake_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/Player/snake/snake_right_2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(){
+
+        spriteCounter++;
+        if (spriteCounter > 10) {
+            if(spriteNum == 1){
+                spriteNum = 2;
+            }
+            else if(spriteNum == 2){
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
+    }
+
+    public void draw(Graphics2D g2){
+        BufferedImage img = null;
+
+        switch(direction){
+            case "up":
+                if(spriteCounter == 1)
+                    img = up1;
+                if(spriteCounter == 2)
+                    img = up2;
+                break;
+            case "down":
+                if(spriteCounter == 1)
+                    img = down1;
+                if(spriteCounter == 2)
+                    img = down2;
+                break;
+            case "left":
+                if(spriteCounter == 1)
+                    img = left1;
+                if(spriteCounter == 2)
+                    img = left2;
+                break;
+            case "right":
+                if(spriteCounter == 1)
+                    img = right1;
+                if(spriteCounter == 2)
+                    img = right2;
+                break;
+        }
+
+        g2.drawImage(img, x, y, j.tileSize, j.tileSize, null);
     }
 }
--- src/main/java/Principal/Janela.java
+++ src/main/java/Principal/Janela.java
@@ -2,38 +2,43 @@ package Principal;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
 
 import javax.swing.JPanel;
+
+import Entidades.Player;
 /**
  *
  * @author caiom
  */
 public class Janela extends JPanel implements Runnable {
+    // config da janela
     final int originalTileSize = 32; // 32 x 32
     final int scale = 2; // 2x32 = 64
-    final int tileSize = originalTileSize * scale; // 64x64
+
+    public final int tileSize = originalTileSize * scale; // 64x64
 
     // Obtendo a resolução da tela automaticamente
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    final int screenWidth = (int) screenSize.getWidth();
-    final int screenHeight = (int) screenSize.getHeight();
+    public final int screenWidth = (int) screenSize.getWidth();
+    public final int screenHeight = (int) screenSize.getHeight();
 
+    // FPS
+    int FPS = 60;
     Thread gameThread;
-    
+    KeyHandler keyH = new KeyHandler();
+    Player player = new Player(this, keyH);
+
     // construtor
     public Janela() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.GREEN);
+        this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
         this.setFocusable(true);
         this.requestFocus();
-    }
-
-
-    @Override
-    public void run(){
-
     }
 
     public void iniciarGameThread() {
@@ -43,12 +48,47 @@ public class Janela extends JPanel implements Runnable {
         }
     }
 
-    // getters
-    public int getScreenWidth() {
-        return screenWidth;
+    @Override
+    public void run(){
+        double drawInterval = 1000000000 / FPS; // 0.01666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null){
+            update();
+
+            repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000; // convertendo para milisegundos
+
+                if(remainingTime < 0){
+                    remainingTime = 0; // Garante que o tempo restante não seja negativo
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public int getScreenHeight() {
-        return screenHeight;
+    public void update(){
+        player.update();
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        player.draw(g2);
+
+        g2.dispose();
     }
 }
